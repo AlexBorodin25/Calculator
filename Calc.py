98821137bb6f073d4bdb19e8b9ab0d5f0aa7c0d5
AlexBorodin25 <alex.borodin2002@gmail.com>
2026-06-14 17:37:57 -0700

    finalized parse_input

--- Calc.py
+++ Calc.py
@@ -12,5 +12,19 @@ def divide(a, b):
 def parse_input(user_input):
     problem = user_input.strip().split()
 
+    left, operator, right = problem
+
     if len(problem) != 3:
         raise ValueError("Invalid input. Try (5 + 2) format.")
+
+    try:
+        left = float(left)
+        right = float(right)
+    except ValueError:
+        raise ValueError("Invalid input. Try using numbers.")
+
+    if operator not in ["+", "-", "x", "÷"]:
+        raise ValueError("Invalid operator. Use +, -, x, ÷")
+
+    return left, operator, right
+
